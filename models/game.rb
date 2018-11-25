a8006d57829dfeec7c37c76cc3e2c24b3dca8572
Iain Floyd <39953553+floyd924@users.noreply.github.com>
2018-11-25 14:37:09 +0000

    refactor model

--- models/game.rb
+++ models/game.rb
@@ -16,30 +16,6 @@ class Game
     else
       return "Ehhhh make sure you typed name / rps / name / rps"
 
-      # if hand1 == "rock" && hand2 == "scissors"
-      #   return "#{player1} wins because rock beats scissors"
-      #
-      # elsif hand1 == "rock" && hand2 == "paper"
-      #   return "#{player2} wins because paper beats rock"
-      #
-      # elsif hand1 == "paper" && hand2 == "scissors"
-      #   return "#{player2} wins because scissors beats paper"
-      #
-      # elsif hand1 == "paper" && hand2 == "rock"
-      #   return "#{player1} wins because paper beats rock"
-      #
-      # elsif hand1 == "scissors" && hand2 == "paper"
-      #   return "#{player1} wins because scissors beats paper"
-      #
-      # elsif hand1 == "scissors" && hand2 == "rock"
-      #   return "#{player2} wins because rock beats scissors"
-      #
-      # elsif hand1 == hand2
-      #   return "It's a draw"
-      # else
-      #   return "You are a fucking moron, this is wrong paper, scissors"
-
-
     end
   end
 
